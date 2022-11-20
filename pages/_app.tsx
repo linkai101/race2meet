f665e6098bab6060152284d1e5de85efd11e245e
tinuh <tinuvanapamula@gmail.com>
2022-11-20 01:34:01 -0500

    persistent peerid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,61 @@
-import '../styles/globals.css';
+import React, { useEffect, useState, useRef } from "react";
+import "../styles/globals.css";
 
-import Head from 'next/head';
-import { AppProps } from 'next/app';
+import Head from "next/head";
+import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+	const [loading, setLoading] = useState<string>("Generating Peer ID ...");
+	const [stream, setStream] = useState<MediaStream>();
 
-    <Component {...pageProps} />
-  </>;
+	const myVideo = useRef<HTMLVideoElement>(null);
+	const theirVideo = useRef<HTMLVideoElement>(null);
+
+	useEffect(() => {
+		let peerid = localStorage.getItem("peerid");
+
+		import("peerjs").then(async ({ default: Peer }) => {
+			const peer = peerid ? await new Peer(peerid) : await new Peer();
+
+			peer.on("open", function (id) {
+				console.log("My peer ID is: " + id);
+				setLoading("Awaiting Connection ...");
+				localStorage.setItem("peerid", id);
+			});
+
+			let navigator = window.navigator as any;
+
+			var getUserMedia = navigator.mediaDevices.getUserMedia;
+
+			getUserMedia({ video: true }).then((stream: MediaStream) => {
+				setStream(stream);
+				if (myVideo.current) {
+					myVideo.current.srcObject = stream;
+				}
+			});
+			peer.on("call", function (call) {
+				console.log("Getting a call...");
+				getUserMedia({ video: true }).then((stream: MediaStream) => {
+					call.answer(stream);
+					call.on("stream", function (remoteStream) {
+						if (theirVideo.current) {
+							theirVideo.current.srcObject = remoteStream;
+						}
+					});
+				});
+			});
+		});
+	}, []);
+
+	return (
+		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<Component {...pageProps} myVideo={myVideo} theirVideo={theirVideo} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
