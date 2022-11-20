3e9e0a2a002dfd685f4bdde88d29ae948f8d58c8
Linkai Wu <linkai@linkaiwu.com>
2022-11-20 02:28:23 -0500

    Add psudocode for game page

--- pages/game.tsx
+++ pages/game.tsx
@@ -7,6 +7,13 @@ export default function Game({
 	myVideo: React.RefObject<HTMLVideoElement>;
 	theirVideo: React.RefObject<HTMLVideoElement>;
 }) {
+	/*
+		1. join game (airtable.ts joinGame)
+		2. get unpaired players (airtable.ts getUnpairedPlayers)
+		3. pair with first unpaired player (airtable.ts pairPlayers)
+		4. if it doesn't work, handle (airtable.js handleFailedPairing) and go back to 2, if none are available then wait
+	*/
+
 	return (
 		<div>
 			<video ref={myVideo} autoPlay className="rounded-lg w-96"></video>
